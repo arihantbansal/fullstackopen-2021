24e69807f4c94e111349694268ceeee19a808f2a
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-07-19 20:43:43 +0530

    lint

--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Filter = ({ filterContent }) => {
-  return (
-    <div>
-      <label className="label">Filter shown with</label>
-      <input className="input" onChange={filterContent} />
-    </div>
-  );
+	return (
+		<div>
+			<label className="label">Filter shown with</label>
+			<input className="input" onChange={filterContent} />
+		</div>
+	);
 };
 
 export default Filter;
