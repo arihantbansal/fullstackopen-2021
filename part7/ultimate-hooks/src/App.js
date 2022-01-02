285e27b07a1748d2c536ac5bef1fc2cb172588d1
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-01-02 15:13:31 +0530

    Async fix

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -23,7 +23,7 @@ const useField = type => {
 const useResource = baseUrl => {
 	const [resources, setResources] = useState([]);
 
-	const create = resource => {
+	const create = async resource => {
 		const response = await axios.post(baseUrl, resource);
 		setResources([...resources, response.data]);
 	};
