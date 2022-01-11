d13aeada1513135bfb41d24a045c7bfcd3284cb4
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-01-11 11:29:02 +0530

    Exercise 8.13-8.14

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,99 +1,31 @@
 const { ApolloServer, UserInputError, gql } = require("apollo-server");
+const mongoose = require("mongoose");
+const Author = require("./models/author");
+const Book = require("./models/book");
 const {
 	ApolloServerPluginLandingPageGraphQLPlayground,
+	AuthenticationError,
 } = require("apollo-server-core");
 const { v1: uuid } = require("uuid");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-let authors = [
-	{
-		name: "Robert Martin",
-		id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-		born: 1952,
-	},
-	{
-		name: "Martin Fowler",
-		id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-		born: 1963,
-	},
-	{
-		name: "Fyodor Dostoevsky",
-		id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-		born: 1821,
-	},
-	{
-		name: "Joshua Kerievsky", // birthyear not known
-		id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-	},
-	{
-		name: "Sandi Metz", // birthyear not known
-		id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-	},
-];
-
-/*
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- */
-
-let books = [
-	{
-		title: "Clean Code",
-		published: 2008,
-		author: "Robert Martin",
-		id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring"],
-	},
-	{
-		title: "Agile software development",
-		published: 2002,
-		author: "Robert Martin",
-		id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-		genres: ["agile", "patterns", "design"],
-	},
-	{
-		title: "Refactoring, edition 2",
-		published: 2018,
-		author: "Martin Fowler",
-		id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring"],
-	},
-	{
-		title: "Refactoring to patterns",
-		published: 2008,
-		author: "Joshua Kerievsky",
-		id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring", "patterns"],
-	},
-	{
-		title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-		published: 2012,
-		author: "Sandi Metz",
-		id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-		genres: ["refactoring", "design"],
-	},
-	{
-		title: "Crime and punishment",
-		published: 1866,
-		author: "Fyodor Dostoevsky",
-		id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-		genres: ["classic", "crime"],
-	},
-	{
-		title: "The Demon ",
-		published: 1872,
-		author: "Fyodor Dostoevsky",
-		id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-		genres: ["classic", "revolution"],
-	},
-];
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log("connected to MongoDB");
+	})
+	.catch(error => {
+		console.log("error connection to MongoDB:", error.message);
+	});
 
 const typeDefs = gql`
 	type Book {
 		title: String!
 		published: Int!
-		genres: [String!]
-		author: String!
+		genres: [String!]!
+		author: Author!
+		id: ID!
 	}
 
 	type Author {
@@ -122,28 +54,23 @@ const typeDefs = gql`
 
 const resolvers = {
 	Query: {
-		bookCount: () => books.length,
-		authorCount: () => authors.length,
+		bookCount: () => Book.collection.countDocuments(),
+		authorCount: () => Author.collection.countDocuments(),
 		allBooks: (root, args) => {
-			const byGenre = book => book.genres.includes(args.genre);
-			const byAuthor = book => (args.author === book.author ? book : null);
-
-			if (!args.author) {
-				if (!args.genre) return books;
-				return books.filter(byGenre);
-			}
+			// const byGenre = book => book.genres.includes(args.genre);
+			// const byAuthor = book => (args.author === book.author ? book : null);
 
 			if (!args.genre) {
-				return books.filter(byAuthor);
+				return Book.find({});
 			}
 
-			return books.filter(byAuthor).filter(byGenre);
+			return Book.find({ genres: { $in: [genre] } });
 		},
-		allAuthors: () => authors,
+		allAuthors: () => Author.find({}),
 	},
 	Author: {
 		bookCount: root => {
-			const booksByAuthor = books.filter(b => b.author === root.name);
+			const booksByAuthor = await Book.find({ author: root.name });
 			return booksByAuthor.length;
 		},
 	},
